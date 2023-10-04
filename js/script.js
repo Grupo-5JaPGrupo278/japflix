8269d65dfbf39e133b22f92be25c5d78c8278cbe
Franco Echaide <echaidefranco@gmail.com>
2023-10-04 20:10:50 -0300

    estrellita estrellita

--- js/script.js
+++ js/script.js
@@ -1,53 +1,80 @@
-const MOVIES_URL = 'https://japceibal.github.io/japflix_api/movies-data.json';
-const INPUT = document.getElementById('textArea');
-const CONTAINER = document.getElementById('itemContainer');
-const SEARCHBTN = document.getElementById('searchBtn');
+const MOVIES_URL = "https://japceibal.github.io/japflix_api/movies-data.json";
+const INPUT = document.getElementById("textArea");
+const CONTAINER = document.getElementById("itemContainer");
+const SEARCHBTN = document.getElementById("searchBtn");
 
 let movies = [];
 let filteredmovies = [];
 function getMovies(url) {
-    return fetch(url)
-        .then(response => {
-            if (!response.ok) {
-                throw new Error("Network response was not ok");
-            }
-            return response.json();
-        })
-        .catch(err => {
-            console.error("Error:", err);
-            console.log("err");
-        });
+	return fetch(url)
+		.then(response => {
+			if (!response.ok) {
+				throw new Error("Network response was not ok");
+			}
+			return response.json();
+		})
+		.catch(err => {
+			console.error("Error:", err);
+			console.log("err");
+		});
 }
-document.addEventListener('DOMContentLoaded', (e) => {
-    getMovies(MOVIES_URL).then(data => {
-        movies = data;
-        console.log(data)
-    })
-})
-function showMovies(){
-    let HTMLContentToAppend = ''
-    for (let i = 0; i < filteredmovies.length; i++){
-        HTMLContentToAppend += `
+document.addEventListener("DOMContentLoaded", e => {
+	getMovies(MOVIES_URL).then(data => {
+		movies = data;
+		console.log(data);
+	});
+});
+function showMovies() {
+	let HTMLContentToAppend = "";
+
+	for (let i = 0; i < filteredmovies.length; i++) {
+		HTMLContentToAppend += `
         <div class="peliContainer">
             <div class="peliInfo">
                 <div class="peliTitle">${filteredmovies[i].title}</div>
                 <div class="peliTagline">${filteredmovies[i].tagline}</div>
             </div>
-            <div>${filteredmovies[i].vote_average}</div>
+            <div>
+            <span class="fa fa-star star"></span>
+            <span class="fa fa-star star"></span>
+            <span class="fa fa-star star"></span>
+            <span class="fa fa-star star"></span>
+            <span class="fa fa-star star"></span>
+            <span class="fa fa-star star"></span>
+            <span class="fa fa-star star"></span>
+            <span class="fa fa-star star"></span>
+            <span class="fa fa-star star"></span>
+            </div>
         </div>
-        `
-    }
-    // : title, tagline, y vote_average
-    CONTAINER.innerHTML = HTMLContentToAppend
-    
+        `;
+	}
+	CONTAINER.innerHTML = HTMLContentToAppend;
+
+	let pelis = document.getElementsByClassName("peliContainer");
+
+	for (let i = 0; i < pelis.length; i++) {
+		let star = pelis[i].getElementsByClassName("star");
+		let rating = Math.round(filteredmovies[i].vote_average);
+		for (let j = 0; j < rating; j++) {
+			star[j].classList.add("checked");
+		}
+	}
+
+	// : title, tagline, y vote_average
+
+	//${filteredmovies[i].vote_average}
 }
-SEARCHBTN.addEventListener('click', () => {
-    filteredmovies = [];
-    let inputText = INPUT.value.toLowerCase(); // Convierte el texto a minúsculas para comparar de forma insensible a mayúsculas y minúsculas
-    for (let i = 0; i < movies.length; i++) {
-        if (movies[i].title.toLowerCase().includes(inputText) || movies[i].tagline.toLowerCase().includes(inputText) || movies[i].overview.toLowerCase().includes(inputText)) {
-            filteredmovies.push(movies[i]);
-        }
-    }
-    showMovies(); // Llama a la función para mostrar las películas filtradas
+SEARCHBTN.addEventListener("click", () => {
+	filteredmovies = [];
+	let inputText = INPUT.value.toLowerCase(); // Convierte el texto a minúsculas para comparar de forma insensible a mayúsculas y minúsculas
+	for (let i = 0; i < movies.length; i++) {
+		if (
+			movies[i].title.toLowerCase().includes(inputText) ||
+			movies[i].tagline.toLowerCase().includes(inputText) ||
+			movies[i].overview.toLowerCase().includes(inputText)
+		) {
+			filteredmovies.push(movies[i]);
+		}
+	}
+	showMovies(); // Llama a la función para mostrar las películas filtradas
 });
